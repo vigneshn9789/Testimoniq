20e64396656043e6ae8c5e8d941ec2ee3babf17e
vigneshn <vigneshn9789@gmail.com>
2024-10-12 08:17:03 +0530

    railway DB  change

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,30 +2,38 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-});
+//for-----local
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// //   waitForConnections: true,
+// //   connectionLimit: 10,
+// //   queueLimit: 0,
+// });
 
 
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Passcode@123',
-//     database: 'VProject',
-//   });
+// // const db = mysql.createConnection({
+// //     host: 'localhost',
+// //     user: 'root',
+// //     password: 'Passcode@123',
+// //     database: 'VProject',
+// //   });
  
 
-// we use below only when using createConnection()
+// // we use below only when using createConnection()
 
-// db.connect((err) => {
-//     if (err) throw err;
-//     console.log('Database connected successsfully');
-//   });
+// // db.connect((err) => {
+// //     if (err) throw err;
+// //     console.log('Database connected successsfully');
+// //   });
+
+
+
+
+//----------------for railway
+const db = mysql.createPool(process.env.DB_URL);
 
 module.exports = db;
